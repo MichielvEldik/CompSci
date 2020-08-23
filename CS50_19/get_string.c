d732fec5da0279e9918bdb111a25455d129efb26
Mitchy123 <michiel_eldik@msn.com>
2020-08-23 14:01:26 +0200

    new c files caleb currys tutorial

---
+++ CS50_19/get_string.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+int main(void)
+{
+		string answer = get_string("What's your name?\n");
+		printf("Hello, %s\n", answer);
+}
--- CS50_19/hello_c.c
+++ CS50_19/hello_c.c
@@ -1,7 +1,8 @@
+
 # include <stdio.h>
 int main(void)
 {
-		printf("Hello, World!");
+		printf("Hello, World!\n");
+		retyrb 0;
 }
-
 
